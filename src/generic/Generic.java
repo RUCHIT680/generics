25a5dd09f02154eeb841a78f679c26c8a9decdcf
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-17 20:04:31 +0530

    add

---
+++ src/generic/Generic.java
@@ -0,0 +1,20 @@
+package generic;
+
+public class Generic {
+	
+	public class generic {
+
+		public static <T extends Comparable<T>> T maxint_uc1(T[] intArray) {
+			T max=intArray[0];
+			for (int i=0;i<3;i++) {
+				if (intArray[(int) i].compareTo(max)>0) {
+					max= intArray[i];
+				}
+			}
+			return max;
+		}
+
+
+	}
+
+}
