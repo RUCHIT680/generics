66325234ac0eb1d41ad4b96e899e1760822c25bd
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-17 20:15:05 +0530

    add

--- src/generic/Generic.java
+++ src/generic/Generic.java
@@ -10,13 +10,22 @@ public class Generic {
 		}
 		return max;
 	}
-
-
+	
 	public static <T extends Comparable<T>> T maxfloat_uc2(T[] floatArray) {
 		T max=floatArray[0];
 		for (int i=0;i<3;i++) {
 			if (floatArray[(int) i].compareTo(max)>0) {
 				max= floatArray[i];
+			}
+		}
+		return max;
+	}
+
+	public static <T extends Comparable<T>> T maxString_uc3(T[] stringArray) {
+		T max=stringArray[0];
+		for (int i=0;i<3;i++) {
+			if (stringArray[(int) i].compareTo(max)>0) {
+				max= stringArray[i];
 			}
 		}
 		return max;
